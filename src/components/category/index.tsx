41c0f0f1a02c0fbc2c800aefdfefdc1864b97e4b
ferraricastro <filipecastrosaraiva@gmail.com>
2026-06-22 20:22:31 -0300

    add style variation

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -6,18 +6,21 @@ import { styles } from "./styles"
 
 type Props = PressableProps & {
   name: string
+  isSelected: boolean
   icon: keyof typeof MaterialIcons.glyphMap
 }
 
-export function Category({ name, icon, ...rest }: Props) {
+export function Category({ name, icon, isSelected, ...rest }: Props) {
+  const color = isSelected ? colors.green[300] : colors.gray[400]
+
   return (
     <Pressable style={styles.container} {...rest}>
       <MaterialIcons
         name={icon}
         size={16}
-        color={colors.gray[400]}
+        color={color}
       ></MaterialIcons>
-      <Text style={styles.name}>{name}</Text>
+      <Text style={[styles.name, { color }]}>{name}</Text>
     </Pressable>
   )
 }
